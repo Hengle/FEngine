e6704ba68540589337edb59aa91095acdc944335
houshuo <houshuo3@126.com>
2016-11-10 18:32:50 +0800

    a

---
+++ Collision/NarrowPhase/ConvexPenetrationDepthSolver.cs
@@ -0,0 +1,12 @@
+﻿using MobaGame.FixedMath;
+
+namespace MobaGame.Collision
+{
+    public abstract class ConvexPenetrationDepthSolver
+    {
+        public abstract bool calcPenDepth(SimplexSolverInterface simplexSolver,
+            ConvexShape convexA, ConvexShape convexB,
+            VIntTransform transA, VIntTransform transB,
+            VInt3 v, VInt3 pa, VInt3 pb);
+    }
+}
