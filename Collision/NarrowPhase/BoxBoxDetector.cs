7a5a925ca59db748bb9923dda3a4ae2793df4848
houshuo <houshuo3@126.com>
2016-11-25 11:48:26 +0800

    a

---
+++ Collision/NarrowPhase/BoxBoxDetector.cs
@@ -0,0 +1,23 @@
+﻿using MobaGame.FixedMath;
+using System.Collections.Generic;
+using System;
+
+namespace MobaGame.Collision
+{
+    public class BoxBoxDetector: DiscreteCollisionDetectorInterface
+    {
+        BoxShape box1;
+        BoxShape box2;
+
+        public BoxBoxDetector(BoxShape box1, BoxShape box2)
+        {
+            this.box1 = box1;
+            this.box2 = box2;
+        }
+
+        public override void getClosestPoints(ClosestPointInput input, Result output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
