82d383705dd87bffc7a6ec87d7d26ebb8a05a409
houshuo <houshuo3@126.com>
2016-12-14 18:55:33 +0800

    a

--- Collision/NarrowPhase/BoxBoxDetector.cs
+++ Collision/NarrowPhase/BoxBoxDetector.cs
@@ -6,18 +6,29 @@ namespace MobaGame.Collision
 {
     public class BoxBoxDetector: DiscreteCollisionDetectorInterface
     {
-        BoxShape box1;
-        BoxShape box2;
+        BoxShape polyA;
+        BoxShape polyB;
 
         public BoxBoxDetector(BoxShape box1, BoxShape box2)
         {
-            this.box1 = box1;
-            this.box2 = box2;
+            polyA = box1;
+            polyB = box2;
         }
+
+        // for all 15 possible separating axes:
+        //   * see if the axis separates the boxes. if so, return 0.
+        //   * find the depth of the penetration along the separating axis (s2)
+        //   * if this is the mininum depth so far, record it.
+        // the normal vector will be set to the separating axis with the smallest
+        // depth. note: normalR is set to point to a column of R1 or R2 if that is
+        // the smallest depth normal so far. otherwise normalR is 0 and normalC is
+        // set to a vector relative to body 1. invert_normal is 1 if the sign of
+        // the normal should be flipped.
 
         public override void getClosestPoints(ClosestPointInput input, Result output)
         {
             
         }
+
     }
 }
