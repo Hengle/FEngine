25cbf4e74b110a796ead405e3f72040fca38714d
houshuo <houshuo3@126.com>
2016-12-16 09:46:52 +0800

    CollisionMargin改小

--- Framework/Globals.cs
+++ Framework/Globals.cs
@@ -6,7 +6,7 @@ namespace MobaGame.Collision
     {
         public static readonly VFixedPoint EPS = VFixedPoint.FromBinary(256);
         public static readonly VFixedPoint EPS2 = VFixedPoint.FromBinary(16);
-        public static readonly VFixedPoint CONVEX_DISTANCE_MARGIN = VFixedPoint.Create(4) / VFixedPoint.Create(10);
+        public static readonly VFixedPoint CONVEX_DISTANCE_MARGIN = VFixedPoint.Create(4) / VFixedPoint.Create(100);
 
         public static VFixedPoint getContactBreakingThreshold()
         {
